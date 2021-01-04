a6847c7be26be80600c7c3e4dfc87961bf1cb7cb
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2021-01-05 08:56:22 +0900

    primitive

--- src/basic/primitive.ts
+++ src/basic/primitive.ts
@@ -1,4 +1,13 @@
 export default function primitiveSample() {
     const name = 'ストリング'
     console.log(typeof name, name)
+
+    const age = 28
+    console.log(typeof age, age)
+
+    const isSingle = true
+    console.log(typeof isSingle, isSingle)
+
+    const isOver20: boolean = age >= 20
+    console.log(typeof isOver20, isOver20)
 }
