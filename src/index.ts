e5eaa00a2b1108301818d7372560257876a9ca92
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2021-01-05 08:51:55 +0900

    sample

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-import World from './world'
+// import World from './world'
 
-const root: HTMLElement | null = document.getElementById('root')
-const world = new World('Hello World!!!')
-world.sayHello(root)
+// const root: HTMLElement | null = document.getElementById('root')
+// const world = new World('Hello World!!!')
+// world.sayHello(root)
+
+import { primitiveSample } from './basic'
+
+primitiveSample()
