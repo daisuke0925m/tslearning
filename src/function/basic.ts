cb548bb605cd6d9ed2509ae28eabd141d3f77008
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2021-01-08 10:06:24 +0900

    some functions

--- src/function/basic.ts
+++ src/function/basic.ts
@@ -1,3 +1,13 @@
 export const logMessage = (message: string): void => {
     console.log('Function basic sample 1:', message)
 }
+
+export function logMessage2(message: string): void {
+    console.log('Function basic sample 2:', message)
+}
+
+export const logMessage3 = function (message: string): void {
+    console.log('Function basic sample 3:', message)
+}
+
+export const logMessage4 = (message: string): void => console.log('Function basic sample 1:', message)
