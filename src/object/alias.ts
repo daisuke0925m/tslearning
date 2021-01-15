cbbbbd8191ce0aa0558119167760eb0eee974ed0
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2021-01-15 09:07:08 +0900

    upd

--- src/object/alias.ts
+++ src/object/alias.ts
@@ -18,4 +18,39 @@ export default function typeAliasSample() {
         name: 'usa',
     }
     console.log(usa)
+    // ---------------------------------
+    type Knight = {
+        hp: number
+        sp: number
+        weapon: string
+        swordSkill: string
+    }
+
+    type Wizard = {
+        hp: number
+        mp: number
+        weapon: string
+        magicSkill: string
+    }
+
+    // 合併型
+    type Adventurer = Knight | Wizard
+    // 交差型
+    type Paladin = Knight & Wizard
+
+    const adventurer1: Adventurer = {
+        hp: 100,
+        sp: 30,
+        weapon: '木の棒',
+        swordSkill: '三連斬り',
+    }
+    const adventurer2: Adventurer = {
+        hp: 80,
+        mp: 30,
+        weapon: '木の杖',
+        magicSkill: 'ファイアーボール',
+    }
+
+    console.log(adventurer1)
+    console.log(adventurer2)
 }
