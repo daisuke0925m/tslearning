d6afd64c0c2ba46e1cb1830a89acf2a175497c12
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2021-01-16 21:57:16 +0900

    upd

--- src/generics/advanced.ts
+++ src/generics/advanced.ts
@@ -11,4 +11,16 @@ export default function genericsAdvancedSample() {
 
     const numbers = mapStringsToNumbers(['123', '456'], (item) => Number(item))
     console.log(numbers)
+
+    const mapNumbersToNumbers: Map<number, string> = (array, fn) => {
+        const result = []
+        for (let i = 0; i < array.length; i++) {
+            const item = array[i]
+            result[i] = fn(item)
+        }
+        return result
+    }
+
+    const strings = mapNumbersToNumbers([123, 456], (item) => String(item))
+    console.log(strings)
 }
