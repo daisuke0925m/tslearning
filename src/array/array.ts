281a836562f008133a9f97e8947e3edec6b34cb7
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2021-01-16 21:11:49 +0900

    upd

--- src/array/array.ts
+++ src/array/array.ts
@@ -6,4 +6,15 @@ export default function arraySample() {
     const even: Array<number> = [2, 3, 4]
     even.push(1, 2)
     console.log(even)
+
+    const generateSomeArray = () => {
+        const _someArray = []
+        _someArray.push(123)
+        _someArray.push('string')
+        // _someArray.push(true)
+        return _someArray
+    }
+    const someArray = generateSomeArray()
+    // someArray.push(false)
+    console.log(someArray)
 }
