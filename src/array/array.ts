4c5be7d1c01212118d3de5a2ffe04cf43fee6cd8
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2021-01-16 21:07:35 +0900

    upd

--- src/array/array.ts
+++ src/array/array.ts
@@ -2,4 +2,8 @@ export default function arraySample() {
     const colors: string[] = ['red', 'blue']
     colors.push('yellow')
     console.log(colors)
+
+    const even: Array<number> = [2, 3, 4]
+    even.push(1, 2)
+    console.log(even)
 }
