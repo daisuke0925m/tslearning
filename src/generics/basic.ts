13f3a51e9cf1813627d73fcfa41eda69b79d5dd3
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2021-01-16 21:43:27 +0900

    upd

--- src/generics/basic.ts
+++ src/generics/basic.ts
@@ -16,4 +16,23 @@ export default function genericsBasicsSample() {
         return result
     }
     console.log(numberReduce([1, 1], 1))
+
+    type Reduce = {
+        (array: string[], initialValue: string): string
+        (array: number[], initialValue: number): number
+    }
+
+    type GenericReduce<T> = {
+        (array: T[], initialValue: T): T
+    }
+
+    const genericStringReduce: GenericReduce<string> = (array, initialValue) => {
+        let result = initialValue
+        for (let i = 0; i < array.length; i++) {
+            result += array[i]
+        }
+        return result
+    }
+
+    console.log(genericStringReduce(['1', '1'], '1'))
 }
