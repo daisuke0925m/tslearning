110287bce95587bd8ab272906aa18f6419d7c495
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2021-01-31 10:40:17 +0900

    upd

--- src/interface/interface.ts
+++ src/interface/interface.ts
@@ -10,3 +10,21 @@ const francePan: Bread = {
     calories: 300,
     type: 'hard',
 }
+
+type MaboDofu = {
+    calories: number
+    spicyLevel: number
+}
+
+type Rice = {
+    calories: number
+    gram: number
+}
+
+type MaboDon = MaboDofu & Rice
+
+const maboDon: MaboDon = {
+    calories: 500,
+    spicyLevel: 10,
+    gram: 350,
+}
