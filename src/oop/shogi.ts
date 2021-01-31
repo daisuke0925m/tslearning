76c68512b25400d18d3bc3ada6da119729c317e8
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2021-01-31 10:19:27 +0900

    upd

--- src/oop/shogi.ts
+++ src/oop/shogi.ts
@@ -5,6 +5,15 @@ type Dan = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
 class Position {
     // private修飾子のついたプロパティは、Positionクラスから生まれたインスタンスのみで使用できる
     constructor(private suji: Suji, private dan: Dan) {}
+
+    distanceForm(position: Position, player: Player) {
+        if (player == 'first') {
+            return {
+                suji: Math.abs(position.suji - this.suji),
+                dan: Math.abs(Number(position.dan) - Number(this.dan)),
+            }
+        }
+    }
 }
 
 abstract class Piece {
@@ -17,4 +26,6 @@ abstract class Piece {
     moveTo(position: Position) {
         this.position = position
     }
+
+    abstract canMoveTo(position: Position, player: Player): boolean
 }
